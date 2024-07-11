2a3dba7aacc675e392fcd569fdc00e2c4908260b
Miguel's Crazy Sandbox <87053584+MiguelKingofcodes@users.noreply.github.com>
2024-07-10 21:30:20 -0300

    Add files via upload

---
+++ SP_INGRESSOS/Testes de Registro/Register_page - Copia.js
@@ -0,0 +1,19 @@
+
+function Cadastro(){
+    // Variaveis do cadastro...
+    let email, nome, sobreNome, senha, confirmarSenha, telefone;
+
+    // Obtendo e validando logica...
+    email = document.getElementById("emailInput").value;
+    nome = document.getElementById("emailInput").value;
+    sobreNome = document.getElementById("emailInput").value;
+    senha = document.getElementById("emailInput").value;
+    confirmarSenha = document.getElementById("emailInput").value;
+    telefone = document.getElementById("emailInput").value;
+
+}
+
+
+function ConfirmarEmail(){
+
+}
